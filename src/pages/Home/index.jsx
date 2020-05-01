181343defc196bfc80bd324513f9eae5286b50ab
yaokailun <yaokailun@cvte.com>
2020-05-02 00:33:56 +0800

    [F] 尝试修复底部菜单不固定底部问题

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,21 +4,17 @@ import utils from 'utils';
 
 export default class extends React.Component {
   state = {
-    showApp: false,
+    showToolbar: true,
   }
   componentDidMount() {
     this.$f7ready((f7) => {
       const token = utils.getLStorage('MOON_H5_TOKEN');
     });
 
-    this.setState({
-      showApp: true,
-    })
+    window.dispatchEvent(new Event('resize'));
   }
 
   render() {
-    if (!this.state.showApp) return null;
-    console.log('redraw');
     return (
       <Page name="home">
         <Views tabs className="safe-areas">
@@ -27,13 +23,17 @@ export default class extends React.Component {
           <View id="view-trade" name="交易" tab url="/trade/" />
           <View id="view-history" name="历史" tab url="/history/" />
           <View id="view-settings" name="设置" tab url="/settings/" />
-          <Toolbar tabbar labels bottom className="app-tabbar">
-            <Link tabLink="#view-market" tabLinkActive icon="market-icon" text="行情" />
-            <Link tabLink="#view-chart" icon="chart-icon" text="图表" />
-            <Link tabLink="#view-trade"  icon="trade-icon" text="交易" />
-            <Link tabLink="#view-history" icon="history-icon" text="历史" />
-            <Link tabLink="#view-settings" icon="settings-icon" text="设置" />
-          </Toolbar>
+          {
+            this.state.showToolbar && (
+              <Toolbar tabbar labels bottom className="app-tabbar">
+                <Link tabLink="#view-market" tabLinkActive icon="market-icon" text="行情" />
+                <Link tabLink="#view-chart" icon="chart-icon" text="图表" />
+                <Link tabLink="#view-trade"  icon="trade-icon" text="交易" />
+                <Link tabLink="#view-history" icon="history-icon" text="历史" />
+                <Link tabLink="#view-settings" icon="settings-icon" text="设置" />
+              </Toolbar>
+            )
+          }
         </Views>
       </Page>
     )
