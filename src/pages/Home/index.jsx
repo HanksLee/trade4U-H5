e3af109630e8b45a75ca1deeb8ca04df5a2b7a86
yaokailun <yaokailun@cvte.com>
2020-05-01 23:58:25 +0800

    [F] 尝试修复底部菜单不固定底部问题

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,37 +3,39 @@ import { Page, Views, View, Toolbar, Link, LoginScreen } from 'framework7-react'
 import utils from 'utils';
 
 export default class extends React.Component {
+  state = {
+    showToolbar: false,
+  }
   componentDidMount() {
     this.$f7ready((f7) => {
       const token = utils.getLStorage('MOON_H5_TOKEN');
-      // this.$f7router.navigate('/trade/2/', {
-      //   props: {
-      //     mode: 'add',
-      //   }
-      // });
-      // if (!token) {
-      //   this.$f7router.navigate('/trade/0/');
-      // }
     });
+
+    this.setState({
+      showToolbar: true,
+    })
   }
 
   render() {
     return (
       <Page name="home">
         <Views tabs className="safe-areas">
-          <Toolbar tabbar labels bottom className="app-tabbar">
-            <Link tabLink="#view-market" tabLinkActive icon="market-icon" text="行情" />
-            <Link tabLink="#view-chart" icon="chart-icon" text="图表" />
-            <Link tabLink="#view-trade"  icon="trade-icon" text="交易" />
-            <Link tabLink="#view-history" icon="history-icon" text="历史" />
-            <Link tabLink="#view-settings" icon="settings-icon" text="设置" />
-          </Toolbar>
-
           <View id="view-market" name="行情" tabActive tab  url="/market/" />
           <View id="view-chart" name="图表" tab url="/chart/" />
           <View id="view-trade" name="交易" tab url="/trade/" />
           <View id="view-history" name="历史" tab url="/history/" />
           <View id="view-settings" name="设置" tab url="/settings/" />
+          {
+            this.state.showToolbar && (
+              <Toolbar tabbar labels bottom className="app-tabbar">
+                <Link tabLink="#view-market" tabLinkActive icon="market-icon" text="行情" />
+                <Link tabLink="#view-chart" icon="chart-icon" text="图表" />
+                <Link tabLink="#view-trade"  icon="trade-icon" text="交易" />
+                <Link tabLink="#view-history" icon="history-icon" text="历史" />
+                <Link tabLink="#view-settings" icon="settings-icon" text="设置" />
+              </Toolbar>
+            )
+          }
         </Views>
       </Page>
     )
