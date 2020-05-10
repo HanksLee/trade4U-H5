6371d8f41bd90c686d37940db486930a1e29d789
yaokailun <yaokailun@cvte.com>
2020-05-10 17:04:20 +0800

    [M] Tweak style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -29,19 +29,19 @@ export default class extends React.Component {
   render() {
     return (
       <Page name="home">
-        <Toolbar tabbar labels bottom className="app-tabbar">
-          <Link tabLink="#view-market" tabLinkActive icon="market-icon" text="行情" />
-          <Link tabLink="#view-chart" icon="chart-icon" text="图表" />
-          <Link tabLink="#view-trade" icon="trade-icon" text="交易" />
-          <Link tabLink="#view-history" icon="history-icon" text="历史" />
-          <Link tabLink="#view-settings" icon="settings-icon" text="设置" />
-        </Toolbar>
         <Views tabs className="safe-areas">
           <View id="view-market" name="行情" tabActive tab url="/market/" />
           <View id="view-chart" name="图表" tab url="/chart" />
           <View id="view-trade" name="交易" tab url="/trade/" />
           <View id="view-history" name="历史" tab url="/history/" />
           <View id="view-settings" name="设置" tab url="/settings/" />
+          <Toolbar tabbar labels bottom className="app-tabbar">
+            <Link tabLink="#view-market" tabLinkActive icon="market-icon" text="行情" />
+            <Link tabLink="#view-chart" icon="chart-icon" text="图表" />
+            <Link tabLink="#view-trade" icon="trade-icon" text="交易" />
+            <Link tabLink="#view-history" icon="history-icon" text="历史" />
+            <Link tabLink="#view-settings" icon="settings-icon" text="设置" />
+          </Toolbar>
         </Views>
       </Page>
     );
