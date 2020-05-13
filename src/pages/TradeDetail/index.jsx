28fbaab40c9d34f3160d4e2bcca991233256bde0
TY <terenyeung@gmail.com>
2020-05-13 22:45:31 +0800

    [F] Fixed up some bugs

--- src/pages/TradeDetail/index.jsx
+++ src/pages/TradeDetail/index.jsx
@@ -285,7 +285,7 @@ export default class extends BaseReact {
       this.wsConnect.onmessage = evt => {
         const msg = evt.data;
         const data = JSON.parse(msg).data;
-        console.log('data', data);
+        // console.log('data', data);
 
         this.updateTrendData(data);
         this.initChart();
@@ -384,24 +384,24 @@ export default class extends BaseReact {
         payload.open_price = priceValue;
     }
 
-    console.log('payload', JSON.stringify(payload));
+    // console.log('payload', JSON.stringify(payload));
 
     // const errMsg = this.getValidation(payload, mode);
 
-    if (errMsg) {
-      return this.$f7.toast.show({
-        text: errMsg,
-        position: 'center',
-        closeTimeout: 2000,
-      });
-    }
+    // if (errMsg) {
+    //   return this.$f7.toast.show({
+    //     text: errMsg,
+    //     position: 'center',
+    //     closeTimeout: 2000,
+    //   });
+    // }
 
     let res;
     if (actionMode == 'add') {
       try {
         res = await this.$api.trade.createTrade(payload);
 
-        console.log('res', res);
+        // console.log('res', res);
         if(res.status == 201) {
           this.$f7.toast.show({
             text: '下单成功',
@@ -503,12 +503,15 @@ export default class extends BaseReact {
         currentShowSymbol,
       }
     } = this.props;
+    console.log('val', val);
+
     val = Number(val);
-    val = Number(this.state.lotsValue) + (val);
+    val = Number(this.state.lotsValue || 0) + (val);
     val = Number(val.toFixed(2));
 
 
-    if (val < currentShowSymbol?.min_lots) {
+
+    if (val < currentShowSymbol?.symbol_display?.min_lots) {
       return
     }
 
@@ -568,7 +571,7 @@ export default class extends BaseReact {
     ) : 0.001;
     // debugger;
 
-    console.log('lossValue', lossValue);
+    console.log('lotsValue', lotsValue);
 
 
     return (
@@ -648,6 +651,9 @@ export default class extends BaseReact {
                   value={lotsValue}
                   color={'black'}
                   onChange={(evt) => {
+
+                    console.log('evt', evt.target.value);
+
                     if (evt.target.value < currentShowSymbol?.min_lots) return;
 
                     this.setState({
