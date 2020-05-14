22a68b8aeeef1f95661828e24147331733ae0cc0
yaokailun <yaokailun@cvte.com>
2020-05-14 13:02:56 +0800

    [A] 显示更多交易产品字段

--- src/pages/Market/SymbolDetail.jsx
+++ src/pages/Market/SymbolDetail.jsx
@@ -40,9 +40,14 @@ export default class extends React.Component {
           )
         }
         <List>
-          <ListItem title="点差" after={symbolDisplay.spread} />
           <ListItem title="小数点位" after={symbolDisplay.decimals_place} />
-          <ListItem title="合约数量" after={symbolDisplay.contract_size} />
+          <ListItem title="合约大小" after={symbolDisplay.contract_size} />
+          <ListItem title="点差" after={symbolDisplay.spread} />
+          <ListItem title="预付款货币" after={symbolDisplay.margin_currency_display} />
+          <ListItem title="获利货币" after={symbolDisplay.profit_currency_display} />
+          <ListItem title="最小交易手数" after={symbolDisplay.min_trading_volume} />
+          <ListItem title="最大交易手数" after={symbolDisplay.max_trading_volume} />
+          <ListItem title="交易数步长" after={symbolDisplay.volume_step} />
         </List>
       </Page>
     );
