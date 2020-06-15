430df12d6b0aedf6524be593c1b55c0cf554a30f
Hank Lee <baf6715@hotmail.com.tw>
2020-06-15 15:56:35 +0800

    adjust domain

--- src/pages/Chart/TVChartContainer/DatafeedProvider.js
+++ src/pages/Chart/TVChartContainer/DatafeedProvider.js
@@ -1,17 +1,17 @@
-import api from 'services';
-import ws from 'utils/ws';
-import { supportedResolution } from 'constant';
+import api from "services";
+import ws from "utils/ws";
+import { supportedResolution } from "constant";
 
 const resolutionMap = {
-  '1': '1m',
-  '5': '5m',
-  '15': '15m',
-  '30': '30m',
-  '60': '1h',
-  '240': '4h',
-  'D': '1d',
-  '1D': '1d',
-  '7D': '7d',
+  "1": "1m",
+  "5": "5m",
+  "15": "15m",
+  "30": "30m",
+  "60": "1h",
+  "240": "4h",
+  "D": "1d",
+  "1D": "1d",
+  "7D": "7d",
 };
 
 export default class DatafeedProvider {
@@ -21,37 +21,37 @@ export default class DatafeedProvider {
   subscriberList = [];
   kChartData = [];
 
-  onReady = cb => {
+  onReady = (cb) => {
     setTimeout(() => {
       cb({
         supported_resolutions: supportedResolution,
-      })
+      });
     }, 0);
-  }
+  };
 
   resolveSymbol = async (symbol, onSymbolResolvedCallback) => {
-    if (symbol === '000') return;
+    if (symbol === "000") return;
     const res = await api.market.getCurrentSymbol(symbol);
-    setTimeout(function() {
+    setTimeout(function () {
       onSymbolResolvedCallback({
         name: symbol,
         ticker: symbol,
         type: res.data.product_details.type,
         description: res.data.symbol_display.description,
         supported_resolutions: supportedResolution,
-        timezone: 'Asia/Shanghai',
-        session: '24x7',
+        timezone: "Asia/Shanghai",
+        session: "24x7",
         minmov: 1,
         pricescale: Math.pow(10, res.data.symbol_display.decimals_place),
         minmove2: 0,
         has_intraday: true,
         // intraday_multipliers: ['1', '60'],
-      })
-    }, 0)
-  }
+      });
+    }, 0);
+  };
 
   getChartData = (trend) => {
-    return trend.map(item => ({
+    return trend.map((item) => ({
       time: item[0] * 1000, //TradingView requires bar time in ms
       low: item[6],
       high: item[5],
@@ -59,10 +59,16 @@ export default class DatafeedProvider {
       close: item[2],
       volume: item[4],
     }));
-  }
+  };
 
-  getBars = async function(symbolInfo, resolution, from, to, onHistoryCallback) {
-    console.log('getBars', symbolInfo, resolution);
+  getBars = async function (
+    symbolInfo,
+    resolution,
+    from,
+    to,
+    onHistoryCallback
+  ) {
+    console.log("getBars", symbolInfo, resolution);
 
     if (!symbolInfo) return;
 
@@ -81,15 +87,17 @@ export default class DatafeedProvider {
     const res = await api.trend.getSymbolTrend(symbolInfo.ticker, {
       params: {
         unit: resolutionMap[resolution],
-      }
-    })
+      },
+    });
 
     const bars = this.getChartData(res.data.trend);
     this.kChartData = bars;
-    onHistoryCallback(bars, { noData: !bars.length, });
-  
+    onHistoryCallback(bars, { noData: !bars.length });
+
     if (this.wsConnect) this.wsConnect.close();
-    this.wsConnect = ws(`symbol/${symbolInfo.ticker}/trend`);
+    this.wsConnect = ws(
+      `symbol/${symbolInfo.ticker}/trend${resolutionMap[resolution]}`
+    );
     this.wsConnect.onmessage = (event) => {
       const message = event.data;
       const data = JSON.parse(message).data;
@@ -104,19 +112,28 @@ export default class DatafeedProvider {
 
       this.subscriberList = this.subscriberList || [];
       for (const sub of this.subscriberList) {
-        if (sub.symbolName !== this.lastSymbolName || sub.resolution !== resolution) {
+        if (
+          sub.symbolName !== this.lastSymbolName ||
+          sub.resolution !== resolution
+        ) {
           this.kChartData = [];
         } else {
-          if (typeof sub.callback !== 'function') return;
+          if (typeof sub.callback !== "function") return;
           sub.callback(formatData);
         }
       }
-    }
-  }
+    };
+  };
 
-  subscribeBars = (symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback) => {
+  subscribeBars = (
+    symbolInfo,
+    resolution,
+    onRealtimeCallback,
+    subscriberUID,
+    onResetCacheNeededCallback
+  ) => {
     this.subscriberList = this.subscriberList || [];
-    const found = this.subscriberList.some(n => n.uid === subscriberUID);
+    const found = this.subscriberList.some((n) => n.uid === subscriberUID);
     if (found) return;
 
     this.subscriberList.push({
@@ -125,12 +142,12 @@ export default class DatafeedProvider {
       uid: subscriberUID,
       callback: onRealtimeCallback,
     });
-  }
+  };
 
   unsubscribeBars = (subscriberUID) => {
     this.subscriberList = this.subscriberList || [];
-    const idx = this.subscriberList.findIndex(n => n.uid === subscriberUID);
+    const idx = this.subscriberList.findIndex((n) => n.uid === subscriberUID);
     if (idx < 0) return;
     this.subscriberList.splice(idx, 1);
-  }
+  };
 }
