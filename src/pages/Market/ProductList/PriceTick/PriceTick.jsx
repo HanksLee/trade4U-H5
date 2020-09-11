d21b5aa224f7e1af0ee7259808210753ea9affbd
liaman <liaman@futengcom.com>
2020-09-11 16:56:06 +0800

    feat: 调整闪灯动画＆吃CSS变数

--- src/pages/Market/ProductList/PriceTick/PriceTick.jsx
+++ src/pages/Market/ProductList/PriceTick/PriceTick.jsx
@@ -13,23 +13,24 @@ export const PriceTick = React.memo(({ buy, sell, change, chg, className }) => {
   const [isLightOn, setIsLightOn] = React.useState(false);
   const lightClass = change > 0 ? "stock-up" : change < 0 ? "stock-down" : null;
   const textClass = change > 0 ? "p-up" : change < 0 ? "p-down" : null;
-
+  const isFirstRender = React.useRef(true);
   React.useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false; // 第一次进页面时不闪灯
+      return;
+    }
     setIsLightOn(true);
-    const id = setTimeout(() => {
-      setIsLightOn(false);
-    }, 2000);
+    const id = setTimeout(() => setIsLightOn(false), 1500);
     return () => {
       clearTimeout(id);
       setIsLightOn(false);
     };
   }, [buy, sell, change, chg]);
+
   return (
     <>
       <div
-        className={cn({
-          "self-select-buy-sell-block": true,
-          [textClass]: true,
+        className={cn("self-select-buy-sell-block", textClass, {
           [lightClass]: isLightOn,
         })}
       >
