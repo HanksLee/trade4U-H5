db986eb8f74a76e36f2ced1ddcfd8c3266a45687
liaman <liaman@futengcom.com>
2020-09-16 16:59:53 +0800

    style: 修正错字

--- src/pages/Market/SymbolInfo/SymbolInfo.jsx
+++ src/pages/Market/SymbolInfo/SymbolInfo.jsx
@@ -62,7 +62,7 @@ export class SymbolInfo extends React.Component {
     const { currentSymbol } = this.props.market;
     const { symbol_display, product_details } = currentSymbol;
     const { quoted_price } = this.props;
-    const onePirceField = {
+    const onePriceField = {
       open: { text: "开盘", format: (val) => String(val) },
       last_close: { text: "昨收", format: (val) => String(val) },
       high: { text: "最高", format: (val) => String(val) },
@@ -96,7 +96,7 @@ export class SymbolInfo extends React.Component {
       selling_fee: { text: "卖出库存费", format: (val) => `${String(val)}%` },
     };
 
-    const field = quoted_price === "one_price" ? onePirceField : twoPriceField;
+    const field = quoted_price === "one_price" ? onePriceField : twoPriceField;
     return (
       <div className={cx("tab-body")}>
         <div className={cx("stock-detail")}>
